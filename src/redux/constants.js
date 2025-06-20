1ae9c7e195ea7023ff35713d92432c1043c1aa74
satoryuk <your_email@example.comthong.vathana.2822@rupp.edu.kh>
2025-06-20 19:32:38 +0700

    main'

--- src/redux/constants.js
+++ src/redux/constants.js
@@ -1,2 +1,4 @@
-export const BASE_URL_LOCAL = "http://127.0.0.1:8000/api/admin";
-export const BASE_PRIVATE_URL = "http://127.0.0.1:8000/api/admin/auth/v1";
+export const BASE_URL_LOCAL =
+  "https://wctbackend-production.up.railway.app/api/admin";
+export const BASE_PRIVATE_URL =
+  "https://wctbackend-production.up.railway.app/api/admin/auth/v1";
--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,15 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
+  server: {
+    proxy: {
+      "/api": {
+        target:
+          "localhost:8000" || "https://wctbackend-production.up.railway.app", // Make sure this matches your backend URL
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
 });
